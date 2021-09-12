26dda643d7f444efdda4b1460642e054ffd961bb
nikolayhotim1 <hotim_nikolay@mail.ru>
2021-09-12 06:10:18 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+'usestrict';
+console.log(Infinity - '1'); // Infinity
+console.log('42' + 42); // '4242'
+console.log(2 + '1 1'); // '21 1'
+console.log(99 + 101); // 200
+console.log('1' - '1'); // 0
+console.log('Result: ' + 10/2) // 'Result: 5'
+console.log(3 + ' bananas ' + 2 + 1 + ' apples'); // '3 bananas 21 apples'
